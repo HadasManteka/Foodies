2817800e60b8e0cdc6def0fcb202a4ce4bd0da0a
Hadas <hadas716@gmail.com>
2023-01-09 19:59:53 +0200

    update recipe + fragment

--- app/src/main/java/com/example/foodies/RecipeDetailsFragment.java
+++ app/src/main/java/com/example/foodies/RecipeDetailsFragment.java
@@ -1,64 +1,90 @@
 package com.example.foodies;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import com.example.foodies.enums.RecipeCategoryEnum;
+import com.example.foodies.enums.RecipeMadeTimeEnum;
+import com.example.foodies.model.Recipe;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecipeDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Display the selected recipe details
  */
-public class RecipeDetailsFragment extends Fragment {
+public class RecipeDetailsFragment extends BaseRecipeFragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Recipe recipe;
 
     public RecipeDetailsFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecipeDetails.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecipeDetailsFragment newInstance(String param1, String param2) {
-        RecipeDetailsFragment fragment = new RecipeDetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static RecipeDetailsFragment newInstance(Recipe recipe){
+        RecipeDetailsFragment frag = new RecipeDetailsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("recipe",recipe);
+        frag.setArguments(bundle);
+        return frag;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        Bundle bundle = getArguments();
+        if (bundle != null){
+            recipe = (Recipe) bundle.getSerializable("recipe");
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_details, container, false);
+    public void setRecipeViewField() {
+        Recipe recipe = new Recipe("cookie", "bake", "5-10 min", "df", "sf", new byte[2]);
+        baseBinding.recipeTitle.setText(recipe.title);
+        baseBinding.recipeIngredients.setText(recipe.ingredients);
+        baseBinding.recipeDescription.setText(recipe.description);
+        baseBinding.imageView2.setImageBitmap(BitmapFactory.decodeByteArray(recipe.recipeImgBytes, 0, recipe.recipeImgBytes.length));
+
+        List<RecipeCategoryEnum> lstCat = new ArrayList<>(Arrays.asList(RecipeCategoryEnum.values().clone()));
+        lstCat.add(0, RecipeCategoryEnum.getCategoryByText(recipe.category));
+        baseBinding.recipeCategory.setAdapter(new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_spinner_item, lstCat));
+
+        List<RecipeMadeTimeEnum> lstTime = new ArrayList<>(Arrays.asList(RecipeMadeTimeEnum.values().clone()));
+        lstTime.add(0, RecipeMadeTimeEnum.getEnumByValue(recipe.time));
+        baseBinding.recipeTime.setAdapter(new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_spinner_item, lstTime));
+
+        setAddImgBtInvisible();
+        setEditMode(false);
+        baseBinding.recipeActionBtn.setText("Edit");
+        baseBinding.recipeAddPicBt.setVisibility(View.INVISIBLE);
+
     }
+
+    @Override
+    public void onClickAction() {
+        // Edit
+    }
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        binding = FragmentRecipeDetailsBinding.inflate(inflater, container, false);
+//        View view = binding.getRoot();
+//
+////        Recipe re = new Recipe("cookie", "bake", "df", "df", "sf", new byte[2]);
+////        FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+////        BaseRecipeFragment recipeFrag = BaseRecipeFragment.newInstance(re, false);
+//////            recipeFrag.setEditMode(false);
+//////            recipeFrag.setAddImgBtInvisible();
+////        ft.replace(binding.fragmentBaseRecipe2.getId(), recipeFrag);
+////        ft.commit();
+//        return view;
+//    }
 }
--- app/src/main/java/com/example/foodies/util/Constants.java
+++ app/src/main/java/com/example/foodies/util/Constants.java
@@ -1,7 +1,7 @@
 package com.example.foodies.util;
 
 public class Constants {
-    public static final String RECIPE_API = "https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&number=30&apiKey=c957b6816ba048139fbc25a67d2cff33";
+    public static final String RECIPE_API = "https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&number=30&apiKey=87719205bae649028b8f294a6de032b6";
 
     public static final int NETWORK_TIMEOUT = 3000;
 }
