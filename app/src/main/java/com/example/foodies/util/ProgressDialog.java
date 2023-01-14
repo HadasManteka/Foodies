6bd7ce32cfbc192991aa29b69e158a01f7ab940c
Hadas <hadas716@gmail.com>
2023-01-14 12:41:06 +0200

    progress base

--- app/src/main/java/com/example/foodies/util/ProgressDialog.java
+++ app/src/main/java/com/example/foodies/util/ProgressDialog.java
@@ -1,15 +1,17 @@
 package com.example.foodies.util;
 
+import android.content.Context;
+
 import com.example.foodies.R;
 
 public class ProgressDialog {
 
     private static android.app.ProgressDialog mProgressDialog;
 
-    public static void showProgressDialog() {
+    public static void showProgressDialog(Context context, String message) {
         if (mProgressDialog == null) {
-            mProgressDialog = new android.app.ProgressDialog(mProgressDialog.getContext());
-            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog = new android.app.ProgressDialog(context);
+            mProgressDialog.setMessage(message);
             mProgressDialog.setIndeterminate(true);
         }
         mProgressDialog.show();
