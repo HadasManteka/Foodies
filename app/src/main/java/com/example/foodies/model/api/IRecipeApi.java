cdd9bd5b48003a2da44f36c8fea188b1159e85f3
Hadas <hadas716@gmail.com>
2023-02-15 23:49:01 +0200

    change number of api get recipe api

--- app/src/main/java/com/example/foodies/model/api/IRecipeApi.java
+++ app/src/main/java/com/example/foodies/model/api/IRecipeApi.java
@@ -4,6 +4,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface IRecipeApi {
-    @GET("/recipes/complexSearch?addRecipeInformation=true&number=50&apiKey=7f6223c950f248008bd0db576dec86ae")
+    @GET("/recipes/complexSearch?addRecipeInformation=true&number=20&apiKey=7f6223c950f248008bd0db576dec86ae")
     Call<RecipesApiResponse> getRecipes();
 }
