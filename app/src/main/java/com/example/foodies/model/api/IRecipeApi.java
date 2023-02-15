7793f175f1118d116f610b56d90833dbc279c459
Hadas <hadas716@gmail.com>
2023-02-15 23:32:50 +0200

    new apikey

--- app/src/main/java/com/example/foodies/model/api/IRecipeApi.java
+++ app/src/main/java/com/example/foodies/model/api/IRecipeApi.java
@@ -4,6 +4,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface IRecipeApi {
-    @GET("/recipes/complexSearch?addRecipeInformation=true&number=50&apiKey=9f6e3c6b334048ae99e030047c371c28")
+    @GET("/recipes/complexSearch?addRecipeInformation=true&number=50&apiKey=7f6223c950f248008bd0db576dec86ae")
     Call<RecipesApiResponse> getRecipes();
 }
